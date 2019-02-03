99c2a8224e89097e210eab77b86717c19e3dbad3
s.soltani <s.soltani@karinaco.com>
2019-02-03 17:26:03 +0330

    composer update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './src/MyApp';
+import MyApp from './src/MyApp';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => MyApp);
